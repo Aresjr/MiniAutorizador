b23769e367105ce3e320e8e607959b72826fa82c
Ares <aresnemeia@gmail.com>
2024-12-14 02:57:27 -0300

    Implementados os testes restantes do TransacoesService

--- src/test/java/br/com/vr/mini_autorizador/service/TransacoesServiceTest.java
+++ src/test/java/br/com/vr/mini_autorizador/service/TransacoesServiceTest.java
@@ -2,10 +2,13 @@ package br.com.vr.mini_autorizador.service;
 
 import br.com.vr.mini_autorizador.dto.SaqueCartaoRequest;
 import br.com.vr.mini_autorizador.enums.MensagemTransacaoCartao;
+import br.com.vr.mini_autorizador.exception.CartaoInexistenteException;
+import br.com.vr.mini_autorizador.exception.SaldoInsuficienteException;
 import br.com.vr.mini_autorizador.exception.SenhaInvalidaCartaoException;
 import br.com.vr.mini_autorizador.model.Cartao;
 import br.com.vr.mini_autorizador.repository.CartaoRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,31 +31,51 @@ class TransacoesServiceTest {
     @Value("${cartao.saldo.inicial:500.00}")
     private BigDecimal VALOR_PADRAO_INICIAL;
 
+    private Cartao cartao;
+
     private final static String NUMERO_CARTAO = "6549873025634501";
+    private final static String NUMERO_CARTAO_INEXISTENTE = "65498730256345011111";
     private final static String SENHA_CARTAO = "1234";
     private final static String SENHA_CARTAO_INVALIDA = "123456";
 
-    @Test
-    void whenRealizarSaqueCartao_shouldReturnOk() {
-        Cartao cartao = new Cartao(null, NUMERO_CARTAO, SENHA_CARTAO, VALOR_PADRAO_INICIAL);
+    @BeforeEach
+    void setup() {
+        cartao = new Cartao(null, NUMERO_CARTAO, SENHA_CARTAO, VALOR_PADRAO_INICIAL);
         Mockito.when(cartaoRepository.findByNumeroCartao(NUMERO_CARTAO))
                 .thenReturn(Optional.of(cartao));
+    }
 
+    @Test
+    void whenRealizarSaqueCartao_shouldReturnOk() {
         SaqueCartaoRequest request = new SaqueCartaoRequest(NUMERO_CARTAO, SENHA_CARTAO, VALOR_PADRAO_INICIAL);
         MensagemTransacaoCartao response = transacoesService.realizarSaqueCartao(request);
         Assertions.assertEquals(response, MensagemTransacaoCartao.OK);
     }
 
     @Test
-    void whenRealizarSaqueCartaoSenhaInvalida_shouldReturnSenhaInvalida() {
-        Cartao cartao = new Cartao(null, NUMERO_CARTAO, SENHA_CARTAO, VALOR_PADRAO_INICIAL);
-        Mockito.when(cartaoRepository.findByNumeroCartao(NUMERO_CARTAO))
-                .thenReturn(Optional.of(cartao));
-
+    void whenRealizarSaqueSenhaInvalida_shouldReturnSenhaInvalida() {
         SaqueCartaoRequest request = new SaqueCartaoRequest(NUMERO_CARTAO, SENHA_CARTAO_INVALIDA, VALOR_PADRAO_INICIAL);
 
         Assertions.assertThrows(SenhaInvalidaCartaoException.class,
                 () -> transacoesService.realizarSaqueCartao(request));
+    }
+
+    @Test
+    void whenRealizarSaqueSaldoInsuficiente_shouldReturnSaldoInsuficiente() {
+        BigDecimal valorAcimaLimite = VALOR_PADRAO_INICIAL.add(new BigDecimal(100));
+        SaqueCartaoRequest request = new SaqueCartaoRequest(NUMERO_CARTAO, SENHA_CARTAO, valorAcimaLimite);
+
+        Assertions.assertThrows(SaldoInsuficienteException.class,
+                () -> transacoesService.realizarSaqueCartao(request));
+    }
+
+    @Test
+    void whenRealizarSaqueCartaoInexistente_shouldReturnCartaoInexistente() {
+        SaqueCartaoRequest request = new SaqueCartaoRequest(NUMERO_CARTAO_INEXISTENTE, SENHA_CARTAO, VALOR_PADRAO_INICIAL);
+
+        Assertions.assertThrows(CartaoInexistenteException.class,
+                () -> transacoesService.realizarSaqueCartao(request));
+
     }
 
 }
